a76027d27862c44866eb192dfed4591ea22ab303
damdaris <darisadam@upi.edu>
2024-09-17 10:43:06 +0700

    Initial Commit

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,102 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Adam Daris Ryadhi on 07/09/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Binding var scrum: DailyScrum
+    
+    @State private var editingScrum = DailyScrum.emptyScrum
+    @State private var isPresentingEditView = false
+    
+    var body: some View {
+        List {
+            Section {
+                NavigationLink(destination: MeetingView(scrum: $scrum)) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            } header: {
+                Text("Meeting Info")
+            }
+            
+            Section {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            } header: {
+                Text("Attendees")
+            }
+            
+            Section {
+                if scrum.history.isEmpty {
+                    Label("No meetings yet", systemImage: "calendar.badge.exclamationmark")
+                }
+                ForEach(scrum.history) { history in
+                    NavigationLink(destination: HistoryView(history: history)) {
+                        HStack {
+                            Image(systemName: "calendar")
+                            Text(history.date, style: .date)
+                        }
+                    }
+                }
+            } header: {
+                Text("History")
+            }
+
+        }
+        .navigationTitle("Design")
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+                editingScrum = scrum
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView, content: {
+            NavigationStack {
+                DetailEditView(scrum: $editingScrum)
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                                scrum = editingScrum
+                            }
+                        }
+                    }
+            }
+        })
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
+}
